d8c91b7eb3089d7b89d6671602a2dd4536cb6a93
M4GNV5 <eragonjml@googlemail.com>
2016-06-22 23:52:18 +0200

    add client shell script that does the same as the client written in js but doesnt require node to be installed

---
+++ client/main.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+file="$HOME/screencapture"
+declare -A config=(
+	["default-mode"]="img_full"
+	["screen-size"]="1920x1080"
+
+	["img-full-resize"]="50%"
+
+	["vid-scale"]="1920/2:-1"
+	["vid-fps"]="30"
+	["upload-url"]="http://127.0.0.1:8080/upload/hunter2"
+
+	["busy-port"]=3112
+)
+
+function img-full
+{
+	file+=".png"
+	gnome-screenshot -f $file
+	convert $file -resize ${config["img-full-resize"]} $file
+}
+
+function img-region
+{
+	file+=".png"
+	gnome-screenshot -a -f "$file"
+}
+
+function clipboard
+{
+	file+=".txt"
+	xclip -selection clipboard -o > "$file"
+}
+
+function cli
+{
+	file+=".${cliFile##*.}"
+	cp $cliFile $file
+}
+
+function vid-full
+{
+	file+=".mp4"
+	avconv -video_size ${config["screen-size"]} -framerate ${config["vid-fps"]} \
+		-f x11grab -i :0.0 -vf scale=${config["vid-scale"]} "$file" &
+	local avconvPid=$!
+	nc -lp ${config["busy-port"]}
+	kill -SIGINT $avconvPid
+	wait $avconvPid
+}
+
+echo "" | nc -q 1 localhost ${config["busy-port"]}
+if [ $? -eq 0 ]; then
+    exit 0
+fi
+
+cliFile=$2
+$1
+url=$(curl -F "file=@$file" ${config["upload-url"]})
+echo "$url" | xclip -selection clipboard
+notify-send -i camera-web "$url"
+rm $file
