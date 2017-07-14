061b7527d8640d8589845dcbd6ed53b15cab0f28
M4GNV5 <jakob@m4gnus.de>
2017-07-14 20:34:33 +0200

    re-add the vid-region functionality

--- client/main.sh
+++ client/main.sh
@@ -46,10 +46,39 @@ function vid-full
 	file+=".mp4"
 	ffmpeg -video_size ${config["screen-size"]} -framerate ${config["vid-fps"]} \
 		-f x11grab -i :1.0 -vf scale=${config["vid-scale"]} "$file" &
-	local avconvPid=$!
+	local ffmpegPid=$!
 	nc -lp ${config["busy-port"]}
-	kill -SIGINT $avconvPid
-	wait $avconvPid
+	kill -SIGINT $ffmpegPid
+	wait $ffmpegPid
+}
+
+function vid-region
+{
+	out=$(xwininfo)
+	#echo "$out"
+
+	startX=$(echo $out | grep -Po "Absolute upper-left X:\s*(\d+)" | cut -d: -f2 | tr -d '[:space:]')
+	startY=$(echo $out | grep -Po "Absolute upper-left Y:\s*(\d+)" | cut -d: -f2 | tr -d '[:space:]')
+	width=$(echo $out | grep -Po "Width:\s*(\d+)" | cut -d: -f2 | tr -d '[:space:]')
+	height=$(echo $out | grep -Po "Height:\s*(\d+)" | cut -d: -f2 | tr -d '[:space:]')
+
+	rem=$(($width % 2))
+	if [[ $rem -eq 1 ]]; then
+		width=$(($width + 1))
+	fi
+
+	rem=$(($height % 2))
+	if [[ $rem -eq 1 ]]; then
+		height=$(($height + 1))
+	fi
+
+	file+=".mp4"
+	ffmpeg -video_size "${width}x${height}" -framerate ${config["vid-fps"]} \
+		-f x11grab -i :1.0+$startX,$startY "$file" &
+	local ffmpegPid=$!
+	nc -lp ${config["busy-port"]}
+	kill -SIGINT $ffmpegPid
+	wait $ffmpegPid
 }
 
 function vid-full-audio
@@ -58,10 +87,10 @@ function vid-full-audio
 	ffmpeg -f pulse -ac 1 -i "${config["microphone"]}" \
 		-f x11grab -r ${config["vid-fps"]} -s ${config["screen-size"]} -i :1.0+0,0 \
 		-acodec aac -vcodec libx264 -vf scale=${config["vid-scale"]} "$file" &
-	local avconvPid=$!
+	local ffmpegPid=$!
 	nc -lp ${config["busy-port"]}
-	kill -SIGINT $avconvPid
-	wait $avconvPid
+	kill -SIGINT $ffmpegPid
+	wait $ffmpegPid
 }
 
 echo "" | nc -q 1 localhost ${config["busy-port"]}
