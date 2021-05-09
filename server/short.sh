71dacc64361fe464bd2d18d02e54fbd2e5d194a9
Jakob Löw <jakob@m4gnus.de>
2021-05-10 01:22:37 +0200

    server: use text.html for displaying and highlighting uploaded text

--- server/short.sh
+++ server/short.sh
@@ -2,14 +2,24 @@
 
 set -e
 
-minutes=$(echo "$DOCUMENT_URI" | grep -Eo '[0-9]+')
+minutes=$(echo "$DOCUMENT_URI" | grep -Eo '[0-9a-fA-F]+' | head -n1)
 ext=$(echo "$DOCUMENT_URI" | cut -d '.' -f 2)
 timestamp=$(((25418760 + $((16#"$minutes"))) * 60))
-filename="/var/www/images/$(date --date "@$timestamp" "+%Y-%m/%Y-%m-%dT%R").$ext"
+
+localname="$(date --date "@$timestamp" "+%Y-%m/%Y-%m-%dT%R")"
+filename="/var/www/i.m4gnus.de/$localname.$ext"
 
 if [ ! -f "$filename" ]; then
-        printf "Content-type: text/plain\n\nInvalid file $DOCUMENT_URI\n"
-        exit 0
+	printf "Content-type: text/plain\n\nInvalid file $DOCUMENT_URI ($filename)\n"
+	exit 0
+fi
+
+if [ "$ext" == "txt" ]; then
+	echo "Status: 303 See Other"
+	echo "Location: /text.html?$localname.txt"
+	echo ""
+	echo ""
+	exit 0
 fi
 
 mime=$(file -b --mime-type "$filename")
